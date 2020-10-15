70bfb02da73776c91d9dd2f81caac97973926fcf
Michael Pfohl <rmpfohl@gmail.com>
2020-10-14 19:48:20 -0500

    fix ride card submission issue

--- src/javascripts/components/cards/rideCards.js
+++ src/javascripts/components/cards/rideCards.js
@@ -1,6 +1,6 @@
 const rideCardBuilder = (rideObject) => {
   const domString = `<div class="card card-body" id="${rideObject.rideId}" style="width: 18rem;">
-  <img src="${rideObject.image}" class="card-img-top" alt="...">
+  <img src="${rideObject.image}" class="card-img-top card-image" alt="...">
   <div>
     <h3 class="card-header">${rideObject.name}</h3>
   </div>
