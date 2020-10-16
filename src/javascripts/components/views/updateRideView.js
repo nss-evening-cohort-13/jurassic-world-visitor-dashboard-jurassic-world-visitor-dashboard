9aa0dc1cd1802168652cb1cab9a7c0947261a9ab
Michael Pfohl <rmpfohl@gmail.com>
2020-10-15 23:32:44 -0500

    make updateRideView into promise

--- src/javascripts/components/views/updateRideView.js
+++ src/javascripts/components/views/updateRideView.js
@@ -1,10 +1,11 @@
 import rideData from '../../helpers/data/rideData';
 import form from '../forms/editRideForm';
 
-const updateRideView = (rideFirebaseKey) => {
-  rideData.getSingleRide(rideFirebaseKey).then((response) => {
-    form.editRideForm(response);
-  });
-};
+const updateRideView = (rideFirebaseKey) => new Promise((resolve, reject) => {
+  rideData.getSingleRide(rideFirebaseKey)
+    .then((response) => {
+      resolve(form.editRideForm(response));
+    }).catch((error) => reject(error));
+});
 
 export default { updateRideView };
