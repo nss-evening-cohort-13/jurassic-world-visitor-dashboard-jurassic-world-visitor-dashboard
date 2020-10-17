372680a8f313b1611bc031b524b7126cf9127faa
Dana Pham <danapham12@gmail.com>
2020-10-17 09:55:21 -0500

    fixed buttons on new cards that are added

--- src/javascripts/components/forms/addDinoForm.js
+++ src/javascripts/components/forms/addDinoForm.js
@@ -38,7 +38,8 @@ const addDinoForm = () => {
               <div>
                 <h3 class="card-text card-header">${data.name}</h3>
               </div>
-              <button type="button" id="${response.data.name}" class="btn btn-info update-dino">Edit</button>
+              <button type="button" id="${response.data.name}" class="btn btn-info update-dino card-btns"><i class="fas fa-pen"></i></button>
+              <button type="button" id="${response.data.name}" class="btn btn-info delete-dino card-btns"><i class="fas fa-trash-alt"></i></button>
             </div>
           </div>`);
           }
