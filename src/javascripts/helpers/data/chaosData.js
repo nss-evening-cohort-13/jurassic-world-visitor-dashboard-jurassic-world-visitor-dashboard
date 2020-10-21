87522228bad2445c0d3943cd2e00d0dde43f409c
Sam Mudick <smudick@gmail.com>
2020-10-20 21:34:38 -0500

    save

--- src/javascripts/helpers/data/chaosData.js
+++ src/javascripts/helpers/data/chaosData.js
@@ -1,25 +1,48 @@
-import equipmentData from './equipmentData';
+// import equipmentData from './equipmentData';
+import axios from 'axios';
 
-const randomEquipment = () => {
-  equipmentData.getEquipment().then((response) => {
-    const arrayLength = response.length;
-    const randomNumber = Math.floor(Math.random() * arrayLength);
-    const brokenEquipment = response[randomNumber].name;
-    console.warn(brokenEquipment);
-    return brokenEquipment;
-  });
-};
+const randomEquipment = (category) => new Promise((resolve, reject) => {
+  axios.get(`https://nutshell-part-two.firebaseio.com/${category}.json`)
+    .then((response) => {
+      console.warn(response);
+      const arrayLength = Object.values(response.data).length;
+      const randomNumber = Math.floor(Math.random() * arrayLength);
+      const choatic = Object.values(response.data)[randomNumber];
+      resolve(choatic.name);
+    }).catch((error) => reject(error));
 
-const chaosMonkey = () => {
-  const category = Math.floor(Math.random() * 3);
-  if (category >= 0) {
-    randomEquipment();
-    console.warn(randomEquipment());
-  } else {
-    console.warn('something else broke');
-  }
-};
+  // equipmentData
+  //   .getEquipment()
+  //   .then((response) => {
+  //     const arrayLength = response.length;
+  //     const randomNumber = Math.floor(Math.random() * arrayLength);
+  //     const brokenEquipment = response[randomNumber];
+  //     resolve(brokenEquipment);
+  //   })
+  //   .catch((error) => reject(error));
+});
 
-console.warn(randomEquipment());
+const chaosMonkey = () => new Promise((resolve, reject) => {
+  const chaosArray = ['staff', 'equipment', 'rides'];
+  const category = chaosArray[Math.floor(Math.random() * 3)];
+
+  // TODO: Update the string/resolve based on the selected category
+  randomEquipment(category).then((response) => {
+    // let selectedCat = ""
+    // if staff
+    //   selectedCat = ...
+    // else if equeueMicrotask...
+    //   ...
+
+
+    resolve(response);
+  }).catch((error) => reject(error));
+
+  // let x;
+  // if (category === 0) {
+  //   x = randomEquipment().then((param) => param);
+  // }
+  // x.then((response) => resolve(response.name)).catch((error) => reject(error));
+});
 
 export default { chaosMonkey };
